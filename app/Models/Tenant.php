88b86a202894fa9a179a29ebebec731c63ecea22
Nanichang Katzing <nanipaul68@gmail.com>
2024-12-23 14:54:28 +0100

    add linting workflow

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -2,11 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 use Stancl\Tenancy\Contracts\TenantWithDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDomains;
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 
 class Tenant extends BaseTenant implements TenantWithDatabase
 {
@@ -30,7 +29,7 @@ class Tenant extends BaseTenant implements TenantWithDatabase
         'encrypt_data',
         'verified',
         'verified_at',
-        'data'
+        'data',
     ];
 
     public static function getCustomColumns(): array
--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, BelongsToTenant;
+    use BelongsToTenant, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
